fc01cd82a7b175e116be1db08d94e8bdeb9bf285
Md Tarek Hossain Tusher <57767316+tarektusher@users.noreply.github.com>
2021-07-07 11:55:18 +0600

    Create LOJ-1170 : Counting Perfect BST.cpp

---
+++ Dynamic Programming/LOJ-1170 : Counting Perfect BST.cpp
@@ -0,0 +1,139 @@
+problem link : https://lightoj.com/problem/counting-perfect-bst
+
+////=====BIsmillahir Rahmanir Rahim =====////
+
+/*                        ______
+ _______   /\     |``\   |        |  /
+    |     /  \    |__/   |____    |/
+    |    / _ _\   |  \   |        |\
+    |   /      \  |   \  |______  |  \
+
+            Dept. of CSE
+         Comilla University
+
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+#define fi 2*acos(0.0)
+#define ll  long long
+#define FIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define Node struct node
+#define spc " "
+#define  E  2.71828182845904523536
+#define pb push_back
+#define pp pop_back
+#define ff first
+#define ss second
+#define valid(nx,ny) nx>=0 && nx<n && ny>=0 && ny<m
+#define edl printf("\n")
+#define infinity 1e16
+#define mod 100000007
+#define cn continue
+#define csprint1 printf("Case %lld: ", cs++)
+#define csprint2 printf("Case %lld:\n", cs++)
+#define sf(a) scanf("%lld", &a)
+#define sff(a,b) scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c) scanf("%lld %lld %lld",&a,&b,&c)
+#define sffff(a,b,c,d) scanf("%lld %lld %lld %lld",&a,&b,&c,&d)
+#define all(v) v.begin(),v.end()
+#define pfn(a) printf("%lld\n",a)
+#define pfs(a) printf("%lld ",a)
+
+using namespace __gnu_pbds;
+using namespace std;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<pl> vpi;
+typedef vector<pl> vpl;
+typedef tree<pair<ll, int> , null_type, less<pair<ll, int> >, rb_tree_tag, tree_order_statistics_node_update> ost;
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << " = " << a <<","<< spc;
+    err(++it, args...);
+    cout<<edl;
+}
+///Bit manipulation
+
+bool checkbit(int mask,int bit){return mask & (1<<bit);}
+int setbit(int mask,int bit){ return mask | (1<<bit) ; }
+int clearbit(int mask,int bit){return mask & ~(1<<bit);}
+int togglebit(int mask,int bit){return mask ^ (1<<bit);}
+int bitno(int mask) {return (int)__builtin_popcount(mask);}
+
+/*----------------------Graph Moves----------------*/
+const int fx[]={+1,-1,+0,+0};
+const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+///=====================================///
+
+#define N 10000000000
+const int maX=1e6+10;
+ll numbers[1000005];
+ll fact[maX];
+
+ll calculate()
+{
+    ll x;ll  cnt=0;
+    for(ll i=2;i<=1000005;i++){
+        x=i*i;
+        while(x<=10000000000){
+            numbers[cnt++]=x;
+            x=x*i;
+        }
+    }
+    sort(numbers,numbers+cnt);
+    cnt=unique(numbers,numbers+cnt)-numbers;
+    numbers[cnt++]=1000000000000000;
+    return cnt;
+}
+void prep()
+{
+    ll ans=1;
+    for(ll i=1;i<=maX;i++){
+        ans=(ans*i)%mod;
+        fact[i]=ans%mod;
+    }
+}
+ll exp(ll a,ll b)
+{
+    if(b==0)return 1;
+    ll x=exp(a,b/2);
+    x=(x*x)%mod;
+    if(b&1)x=(x*a)%mod;
+    return x;
+}
+int main()
+{
+   ll cnt= calculate();
+    prep();
+    ll test;sf(test);
+    ll cs=1;
+    while(test--){
+        ll a,b;
+        sff(a,b);
+        ll r= upper_bound(numbers, numbers+cnt, b)-numbers;
+        ll  l = lower_bound(numbers, numbers+cnt, a)-numbers;
+        ll n=r-l;
+        csprint1;
+
+        if(n==0){
+            pfn(0);
+            continue;
+        }
+        ll x=exp((fact[n+1]*fact[n])%mod,mod-2)%mod;
+        ll y=(fact[2*n]*x)%mod;
+        pfn(y);
+    }
+
+return 0;
+}
